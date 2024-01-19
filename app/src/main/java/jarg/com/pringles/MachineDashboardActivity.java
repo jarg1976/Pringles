75a3f627397977777e17f1f75335614322849e65
Jarg <jarg1976@gmail.com>
2024-01-19 00:42:55 +0000

    Dashboard.

--- app/src/main/java/jarg/com/pringles/MachineDashboardActivity.java
+++ app/src/main/java/jarg/com/pringles/MachineDashboardActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import jarg.com.pringles.models.User;
 
 public class MachineDashboardActivity extends AppCompatActivity {
 
-  private User;
+  private User user;
+  TextView textViewBalanceValue;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_machine_dashboard);
 
     this.user = (User) getIntent().getSerializableExtra("user");
+
+    this.textViewBalanceValue = findViewById(R.id.textView_balance_value);
+
+    this.textViewBalanceValue.setText(String.valueOf(this.user.getBalance()));
+
 
   }
 }
